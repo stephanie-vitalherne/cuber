14e7a24c00b551c2fd4927668e2adbff235f0865
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-11-04 23:00:09 -0400

    header half done

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,19 @@ export default function Home() {
   return (
     <Wrapper>
       <Map />
-      <ActionItems>Start</ActionItems>
+      <ActionItems>
+        {/* Header */}
+        <Header>
+          <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
+          <Profile>
+            <Name>Stephanie Vital-Herne</Name>
+            <UserImage />
+          </Profile>
+        </Header>
+
+        {/* ActionButtons */}
+        {/* InputButtons */}
+      </ActionItems>
     </Wrapper>
   );
 }
@@ -14,6 +26,16 @@ export default function Home() {
 const Wrapper = tw.div`
   flex flex-col h-screen
 `;
+
 const ActionItems = tw.div`
   flex-1
 `;
+
+const Header = tw.div``;
+const UberLogo = tw.img`
+  h-28
+`;
+
+const Profile = tw.div``;
+const Name = tw.div``;
+const UserImage = tw.img``;
